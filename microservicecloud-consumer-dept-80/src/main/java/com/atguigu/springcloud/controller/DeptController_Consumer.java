29a1b59e4f3beebebe115fc573e687051847ae65
liuruibinggit <2675247482@qq.com>
2019-01-08 13:28:55 +0800

    microservice-api

--- microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/controller/DeptController_Consumer.java
+++ microservicecloud-consumer-dept-80/src/main/java/com/atguigu/springcloud/controller/DeptController_Consumer.java
@@ -1,20 +1,27 @@
 package com.atguigu.springcloud.controller;
 
+import java.net.URI;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import com.atguigu.springcloud.entities.Dept;
+import org.springframework.web.util.UriComponents;
+import org.springframework.web.util.UriComponentsBuilder;
 
 @RestController
 public class DeptController_Consumer
 {
 
 	//private static final String REST_URL_PREFIX = "http://localhost:8001";
+	//getForEntity的第一个参数为我要调用的服务的地址，这里我调用了服务提供者提供的/hello接口，注意这里是通过服务名调用而不是服务地址，如果写成服务地址就没法实现客户端负载均衡了
 	private static final String REST_URL_PREFIX = "http://MICROSERVICECLOUD-DEPT";
 
 	/**
@@ -33,6 +40,21 @@ public class DeptController_Consumer
 	@RequestMapping(value = "/consumer/dept/get/{id}")
 	public Dept get(@PathVariable("id") Long id)
 	{
+
+		ResponseEntity<Dept> forEntity = restTemplate.getForEntity(REST_URL_PREFIX + "/test" + id, Dept.class);
+		Dept dept = forEntity.getBody();
+		ResponseEntity<String> forEntity1 = restTemplate.getForEntity(REST_URL_PREFIX + "?name={1}", String.class, Dept.class);
+		String body1 = forEntity1.getBody();
+		Map map = new HashMap();
+		map.put("name","A");
+		ResponseEntity forEntity2 = restTemplate.getForEntity(REST_URL_PREFIX + "?name={name}", String.class, map);
+		restTemplate.delete(REST_URL_PREFIX + "?name={name}", 100);
+		UriComponents uriComponents = UriComponentsBuilder.fromUriString(REST_URL_PREFIX+"?name={name}").build().expand("A").encode();
+		URI uri = uriComponents.toUri();
+
+		ResponseEntity<String> responseEntity = restTemplate.getForEntity(uri, String.class);
+		String body = responseEntity.getBody();
+
 		return restTemplate.getForObject(REST_URL_PREFIX + "/dept/get/" + id, Dept.class);
 	}
 
